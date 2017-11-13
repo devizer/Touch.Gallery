3c12cd7e2f11505432fc1a191bfda659473cc7d4
Volodymyr Hoida <devizer@users.github.com>
2017-11-13 05:49:50 +0300

    Port 8080
    Former-commit-id: ded94e5f84ee3e700b0c73e913de041c2b128666

--- src/publish-and-run-Locally.cmd
+++ src/publish-and-run-Locally.cmd
@@ -1,5 +1,6 @@
 dotnet publish -c Release
 cd bin\Release\netcoreapp2.0\publish
 SET ASPNETCORE_URLS=http://0.0.0.0:6067
+SET ASPNETCORE_URLS=http://+:5000
 start "PRODUCTION Gallery.MVC" dotnet Gallery.MVC.dll 
 rem --urls="http://*:28186"
