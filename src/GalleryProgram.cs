55036164c613d07f1db6a9a287cbad5a2ce679e3
Volodymyr Hoida <devizer@users.github.com>
2017-11-14 00:56:17 +0300

    added travis build
    Former-commit-id: face0484c65d37ebead2c11262607ec40c3112bd

--- src/GalleryProgram.cs
+++ src/GalleryProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,9 +13,15 @@ namespace Gallery.MVC
 {
     public class GalleryProgram
     {
+        static readonly Stopwatch StartAt = Stopwatch.StartNew();
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            // host.Run();
+            host.Start();
+            Console.WriteLine("Start Time: " + StartAt.Elapsed + Environment.NewLine);
+            Console.ReadLine();
+            host.StopAsync().Wait();
         }
 
         public static IWebHost BuildWebHost(string[] args)
