8eedd1930b75d440f751d6c46113f5163c351f3c
Volodymyr Hoida <devizer@users.github.com>
2017-11-18 23:55:47 +0300

    fixed animation position on retina displays
    Former-commit-id: 59336fc7792961052cd0aa238df0b87ee1df9361

--- src/publish-on-APP-Engine.sh
+++ src/publish-on-APP-Engine.sh
@@ -1,3 +1,3 @@
 ﻿#!/bin/bash
-cd $HOME && work=$HOME/Touch.Gallery && rm -rf $work && mkdir -p $work && cd $work && git clone https://github.com/devizer/Touch.Gallery && cd Touch.Gallery/src && dotnet publish -c Release && cd ./bin/Release/netcoreapp2.0/publish/
+cd $HOME && work=$HOME/Touch.Gallery && rm -rf $work && mkdir -p $work && cd $work && git clone https://github.com/devizer/Touch.Gallery && cd Touch.Gallery/src && time (dotnet publish -c Release) && cd ./bin/Release/netcoreapp2.0/publish/
 time (gcloud beta app deploy app.yaml --promote -q --stop-previous-version --verbosity=warning --project=touch-galleries)
