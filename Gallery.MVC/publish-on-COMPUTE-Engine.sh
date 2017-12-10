21d02a3dd27784ffdabb453d03255c8151a6f81b
Volodymyr Hoida <devizer@users.github.com>
2017-12-10 10:20:24 +0300

    Fixed publish-on-COMPUTE-Engine.sh for non-sudo user

--- Gallery.MVC/publish-on-COMPUTE-Engine.sh
+++ Gallery.MVC/publish-on-COMPUTE-Engine.sh
@@ -1,19 +1,25 @@
 ﻿#!/bin/bash
 if [[ -z "$HOME" ]]; then export HOME=/root; fi; mkdir -p "$HOME"
-echo "Booted at $(date)" > $HOME/RESTART.log
+echo "Booted at $(date)" >> $HOME/RESTART.log
+
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
-command -v dotnet >/dev/null 2>&1 && hasDotNet=true
+dotnet --info >/dev/null 2>&1 && hasDotNet=true
 if [ -z "$hasDotNet" ]; then
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
   time (sudo apt-get update && sudo apt-get install -y --allow-unauthenticated iotop mc git htop lsof \
     && sudo apt-get install dotnet-sdk-2.0.3 -y --allow-unauthenticated)
+  echo "Dotnet 2.0.3 installed $(date)" >> $HOME/RESTART.log
 fi
+sudo sync
 
 if [ ! -f "/swap" ]; then
   sudo bash -c 'dd if=/dev/zero of=/swap bs=1M count=1700 && mkswap /swap && swapon /swap'
+  echo "Swap Created $(date)" >> $HOME/RESTART.log
 fi
+sudo mkswap /swap >/dev/null 2>&1 || true
 sudo swapon /swap >/dev/null 2>&1 || true
+sudo sync
 
 work=$HOME/Touch.Gallery
 rm -rf $work
@@ -21,19 +27,21 @@ mkdir -p $work
 cd $work
 git clone https://github.com/devizer/Touch.Gallery Touch.Gallery
 echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
+echo "Src downloaded $(date)" >> $HOME/RESTART.log
 pushd Touch.Gallery
 rm -rf .git
 cd Gallery.MVC
 dotnet publish -c Release -o ../../Touch.Gallery-bin | tee $HOME/BUILD-Touch.Gallery.log
 popd
 rm -rf Touch.Gallery
-# sudo swapoff /swap
+echo "Src builded $(date)" >> $HOME/RESTART.log
+
 
 target=/Touch-Galleries.App
 sudo mkdir -p $target
 ver=$(date +"%Y-%m-%d-%H-%M-%S")
 echo NEW VERSION is: $target/$ver
-time (sudo cp -r Touch.Gallery-bin $target/$ver)
+sudo mv Touch.Gallery-bin $target/$ver
 for prev in $(ls -1 $target | grep -v $ver); do
   echo REMOVING PREV version: $prev
   sudo rm -rf $target/$prev
@@ -45,3 +53,5 @@ echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
 echo "Starting the App at $(date)" >> $HOME/RESTART.log
 # sudo bash -c 'ASPNETCORE_URLS=http://0.0.0.0:80\;http://0.0.0.0:8080\;http://0.0.0.0:5000 dotnet Gallery.MVC.dll' | tee $HOME/RUN-Touch.Gallery.log 2>&1
 (sudo nohup bash -c 'ASPNETCORE_URLS=http://0.0.0.0:80\;http://0.0.0.0:8080\;http://0.0.0.0:5000 dotnet Gallery.MVC.dll' | sudo tee $HOME/RUN-Touch.Gallery.log 2>&1) &
+
+echo "Upgrade finished $(date)" >> $HOME/RESTART.log
