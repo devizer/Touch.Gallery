31724197b14b7e8b743521fbdd5ab75e6df91e86
Volodymyr Hoida <devizer@users.github.com>
2017-12-10 09:35:47 +0300

    Fixed publish-on-COMPUTE-Engine.sh for non-sudo user

---
+++ Gallery.MVC/Initial-Script-On-Compute-Engine.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate -q -nv -O publish-on-COMPUTE-Engine.sh https://github.com/devizer/Touch.Gallery/raw/master/Gallery.MVC/publish-on-COMPUTE-Engine.sh
+bash publish-on-COMPUTE-Engine.sh
