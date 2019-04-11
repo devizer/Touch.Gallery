28ac4f15ddcaab2b0878cffcbed76fc42e13feac
devizer <devizer@users.github.com>
2019-04-11 13:18:37 +0300

    updating

--- Gallery.MVC/Deploy-to-GAE-V2.sh
+++ Gallery.MVC/Deploy-to-GAE-V2.sh
@@ -2,7 +2,6 @@
 set -e 
 REPO=https://github.com/devizer/Touch.Gallery
 
-
 function clear_cache () { echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null; }
 dir=$(basename $REPO)
 work=$HOME/transient-builds
@@ -17,7 +16,7 @@ echo "Src downloaded $(date)"
 cd Gallery.MVC
 dotnet publish -c Release -r linux-x64 --self-contained -o ./bin/Touch.Gallery
 cd bin
-time sudo bash -c 'tar cf - Touch.Gallery | pv | gzip -9 > Touch.Gallery.tar.gz'; ls -la Touch.Gallery.tar.xz
+time sudo bash -c 'tar cf - Touch.Gallery | pv | gzip -9 > Touch.Gallery.tar.gz'; ls -la Touch.Gallery.tar.gz
 rm -rf Touch.Gallery
 gsutil cp Touch.Gallery.tar.gz gs://pet-projects-binaries/
 popd
